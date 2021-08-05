201537301399f9cf07e6bd06cc15fe4934076a0f
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-08-05 14:53:52 +0530

    all pages are served using express

--- index.js
+++ index.js
@@ -4,16 +4,58 @@ const fs = require('fs')
 const app = express()
 
 app.get('/',(req,res) => {
-    // res.send('hi')
+
     fs.readFile('./index.html',(error,data) => {
         if(error) {
-        // res.writeHead(404)
-        res.send('error')    
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('error')
         } else {
-            // res.writeHead(200, {'Content Type': 'text/html'})
-            res.send(data)
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
         }
-        // res.end()
+        res.end()
+    })
+})
+
+app.get('/about',(req,res) => {
+
+    fs.readFile('./about.html',(error,data) => {
+        if(error) {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('error')
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+app.get('/contact',(req,res) => {
+
+    fs.readFile('./contact-me.html',(error,data) => {
+        if(error) {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('error')
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+        }
+        res.end()
+    })
+})
+
+app.get('/*',(req,res) => {
+
+    fs.readFile('./404.html',(error,data) => {
+        if(error) {
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('error')
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.write(data)
+        }
+        res.end()
     })
 })
 
